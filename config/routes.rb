5d6c50279b03f6a131d0681533b6366e8cdcff25
tsuzumi07 <tuzumi.0107@gmail.com>
2019-08-27 20:54:33 +0900

    migrate_error

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-
   get 'home/show'
 
   root to: 'messages#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
 
