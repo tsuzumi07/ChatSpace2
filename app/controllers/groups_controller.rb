9b428599ac863311125e2a8d9829d809e31767f9
tsuzumi07 <tuzumi.0107@gmail.com>
2019-08-30 15:29:37 +0900

    ok_group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only:[:edit, :update]
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -9,30 +9,35 @@ class GroupsController < ApplicationController
     @group.users << current_user
   end
 
+
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to root_path,notice: 'グループを作成しました'
     else
       render :new
     end
- end
-
- def update
-  if @group.update(group_params)
-    redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-  else
-    render :edit
   end
- end
 
- private
- def group_params
-  params.require(:group).permit(:name, user_ids: [] )
- end
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
 
- def set_group
-   @group = Group.find(params[:id])
- end
+  def edit
+    @group = Group.find(params[:id])
+  end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
