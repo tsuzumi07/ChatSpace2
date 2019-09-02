0c22a0506df29c13f57a03cd7edf58bb133294ff
tsuzumi07 <tuzumi.0107@gmail.com>
2019-09-02 20:48:05 +0900

    messagesent

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,22 +5,22 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = @group.messages.includes(:user)
   end
-  
+
   def create
-      @message = @group.messages.new(message_pramas)
-      if @message.save
-        redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-      else
-        @messages = @group.messages.includes(:user)
-        flash.now[:alert] = 'メッセージを入力してください。'
-        render :index
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
 
   private
 
-  def message_pramas
-    paramas.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
   def set_group
