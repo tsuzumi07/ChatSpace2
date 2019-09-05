cc5cd5f4321ede723b6adc246077b7b67b24beda
tsuzumi07 <tuzumi.0107@gmail.com>
2019-09-05 11:16:18 +0900

    ajax_before_up

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,8 +3,8 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
 
-  validates :name, presence: true, uniqueness: true
-
+  validates :name, presence: true
+  
   def show_last_message
     if(last_message = messages.last).present?
       last_message.content? ? last_message.content : '画像が投稿されています'
