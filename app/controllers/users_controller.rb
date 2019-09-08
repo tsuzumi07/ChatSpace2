831dea28aa28fc59262f00094290fc4a6ef9e204
tsuzumi07 <tuzumi.0107@gmail.com>
2019-09-08 15:18:28 +0900

    ok_incremental
    インクメンタルサーチの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def index
+    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:keyword]}%")
+     respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
