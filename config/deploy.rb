bda68a16ec44cfeb764e6c5d60c09451a022267e
tsuzumi07 <tuzumi.0107@gmail.com>
2019-09-13 17:17:30 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -40,7 +38,6 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.11.1'
 
 # Capistranoのログの表示に利用する
 set :application, 'ChatSpace2'
@@ -56,7 +53,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['ssh -i unanekosove.pem'] 
+                  keys: ['~/.ssh/unanekosove.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
