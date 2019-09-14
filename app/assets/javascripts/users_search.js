4d4a3553efe2dd28b6434f55b6df1ebb20808a75
tsuzumi07 <tuzumi.0107@gmail.com>
2019-09-14 20:35:44 +0900

    end

--- app/assets/javascripts/users_search.js
+++ app/assets/javascripts/users_search.js
@@ -5,17 +5,17 @@ $(function() {
   
     function append_assign_list(user_id,user_name){
       var html = `
-      <div class='chat-group-form__field clearfix js-chat-member' id='chat-group-user-8'>
-        <input name='group[user_ids][]' type='hidden' value='${user_id}'>
-        <p class='chat-group-user__name'>${user_name}</p>
-        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-      </div>
+      <div class='chat-group-user'>
+      <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+      <p class='chat-group-user__name'>${ user_name }</p>
+      <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+    </div>
       `
       assign_user_list.append(html);
     }
     function appendList(user){
-      var html = `
-  <div class="chat-group-form__field.clearfix">
+      var html =`
+  <div class="cchat-group-form__field clearfix">
     <p class="chat-group-user__name">${user.name}</p>
     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
   </div>
