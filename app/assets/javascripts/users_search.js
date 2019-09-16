ea35a17f6eb26b9d30702aa66689f0ce6f4e81ae
tsuzumi07 <tuzumi.0107@gmail.com>
2019-09-16 14:50:32 +0900

    correction

--- app/assets/javascripts/users_search.js
+++ app/assets/javascripts/users_search.js
@@ -15,7 +15,7 @@ $(function() {
     }
     function appendList(user){
       var html =`
-  <div class="chat-group-form__field.clearfix">
+  <div class="chat-group-user">
     <p class="chat-group-user__name">${user.name}</p>
     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
   </div>
@@ -24,7 +24,7 @@ $(function() {
     }
 
   function appendNoList(info){
-    var html =`<div class="chat-group-form_field.clearfix>'
+    var html =`<div class="chat-group-user>'
                 <p class="chat-group-user__name">${ info }</p>
               </div>`
 
