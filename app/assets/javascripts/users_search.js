f415861395c8f7c12c0556a89c19a4f3cb6a9659
tsuzumi07 <tuzumi.0107@gmail.com>
2019-09-16 20:08:41 +0900

    Update users_search.js

--- app/assets/javascripts/users_search.js
+++ app/assets/javascripts/users_search.js
@@ -15,10 +15,10 @@ $(function() {
     }
     function appendList(user){
       var html =`
-  <div class="chat-group-user">
-    <p class="chat-group-user__name">${user.name}</p>
-    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-  </div>
+       <div class="chat-group-user">
+       <p class="chat-group-user__name">${user.name}</p>
+       <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+      </div>
       `
       user_list.append(html);
     }
@@ -33,7 +33,9 @@ $(function() {
 
     $(".chat-group-form__input").on("keyup", function() {
       var input = $("#user-search-field").val();
-         
+      
+      
+
       $.ajax({
         type: 'GET',
         url: '/users/',
@@ -53,16 +55,19 @@ $(function() {
             }
           })
           .fail(function(){
-      alert('検索に失敗しました');
+             alert('検索に失敗しました');
           })
     });
+
+   
+
   $(function(){
     $('#user-search-result').on('click','.chat-group-user__btn--add',function(){
       var user_id = $(this).attr('data-user-id');
       var user_name = $(this).attr('data-user-name');
       append_assign_list(user_id,user_name);
       $(this).parent().remove();
-    });
+    }); 
   
     $('#chat-group-users').on('click','.js-remove-btn',function(){
       $(this).parent().remove();
